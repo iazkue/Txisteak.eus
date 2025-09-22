820110a6476307110600337ee3011ce31229676e
iazkue <iazkue@ni.eus>
2025-09-22 17:51:34 +0200

    Githubeko linka gehituta

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-gray-200 text-center p-6 mt-8">
-      <p>&copy; {new Date().getFullYear()} @iazkue. Eskubide guztiak erreserbatuta.</p>
+      <p>&copy; {new Date().getFullYear()} <a target='_blank' href="https://github.com/iazkue/">@iazkue</a> . Eskubide guztiak erreserbatuta.</p>
     </footer>
   );
 };
